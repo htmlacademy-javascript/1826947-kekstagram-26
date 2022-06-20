2b51d80b6f346c1fbe09cf1b9c09027706103660
Kidn3y <79150932621@ya.ru>
2022-06-20 14:05:07 +0300

    Правка ошибок

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function getrandomNumber (minNumer, maxNumber) {
   maxNumber = Math.ceil(maxNumber);   //для вычислкения наибольшего целого числа, которое больше, или равно заданному числу
 
   if (maxNumber <= minNumer) {
-    return ('Пожалуйста выберите число больше' + maxNumber);
+    return (maxNumber++);
   }
 
   return Math.floor(Math.random() * (maxNumber - minNumer)) + minNumer;
