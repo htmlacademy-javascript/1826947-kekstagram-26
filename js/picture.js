2e03db4c99524ea617aaadfff7101ffedc182cfc
Kidn3y <79150932621@ya.ru>
2022-07-04 21:22:30 +0300

    создание функции по отрисовке картинок с данными

--- js/picture.js
+++ js/picture.js
@@ -1,5 +1,3 @@
-import {createPhotos} from './photos.js';
-
 const userDialog = document.querySelector('.pictures');
 
 userDialog.querySelector('.pictures__title').classList.remove('visually-hidden');
@@ -8,14 +6,14 @@ const similarListElement = userDialog.append('section');
 const similarPictureTemplate = document.querySelector('#picture')
   .content.querySelector('.picture');
 
-const drawPicture = createPhotos();
-
-drawPicture.forEach((photo) => {
-  const photoElement = similarPictureTemplate.cloneNode(true);
-  photoElement.querySelector('.picture__img').src = photo.url;
-  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  photoElement.querySelector('.picture__likes').textContent = photo.likes;
-  similarListElement.appendChild(photoElement);
-});
+const drawPicture = function createClone(photos) {
+  photos.forEach((photo) => {
+    const photoElement = similarPictureTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = photo.url;
+    photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    photoElement.querySelector('.picture__likes').textContent = photo.likes;
+    similarListElement.appendChild(photoElement);
+  });
+};
 
 export {drawPicture};
