c336c402872dead4f006de8f0ed6961b4d8c002b
Kidn3y <79150932621@ya.ru>
2022-07-16 17:51:50 +0300

    настройка слайдера

--- js/form.js
+++ js/form.js
@@ -4,7 +4,7 @@ const form = document.querySelector('#upload-select-image');
 
 const uploadField = form.querySelector('#upload-file');
 
-//c onst preview = form.querySelector('.img-upload__preview');
+// const preview = form.querySelector('.img-upload__preview');
 
 const uploadOverlay = form.querySelector('.img-upload__overlay');
 
@@ -13,8 +13,6 @@ const cancelUpload = form.querySelector('#upload-cancel');
 //const scalePhoto = form.querySelector('.img-upload__scale');
 
 //const photoScaleMassive = [0.25, 0.5, 0.75, 1];
-
-//const depthEffect = form.querySelector('.effect-level__value');
 
 const hashtagField = form.querySelector('.text__hashtags');
 
@@ -30,7 +28,7 @@ const pristine = new Pristine(form, {
   errorClass: 'error',
   successClass: 'success',
   errorTextParent: 'img-upload__form',
-  errorTextTag: 'span'
+  errorTextParent: 'spam',
 }, false);
 
 /*const uploadPicture = (picture) => {
@@ -46,27 +44,72 @@ function onEffectChoose (value) {
   return preview.classList.add(`.effects__preview--${value}`);
 }
 
-pristine.addValidator(form.querySelector('[name="effect"]:checked'),
+const chosenEffect = form.querySelector('[name="effect"]:checked');
+
+pristine.addValidator(chosenEffect,
   onEffectChoose, 'Выберите один из эффектов');
 
-function onEffectIntensive (value) {
-  if (value !== 'none') {
-    let min = 0;
-    let max = 1;
-    if (value === 'blur') {
-      max = 3;
-      if (value === 'brightness') {
-        min = 1;
-      }
-    }
-    return value.length >= min && value.length <= max;
-  }
-  return depthEffect.classList.add('.hidden');
-}
+const depthEffect = form.querySelector('.effect-level__value');
 
-pristine.addValidator(form.querySelector('[name="effect-level"]'),
-  onEffectIntensive(form.querySelector('[name="effect"]:checked')),
-  'Выберите интенсивность применения эффекта');
+noUiSlider.create(depthEffect, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return valuetoFixed(1);
+    },
+    from: functuon(value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+depthEffect.noUiSlider.on('update', (...rest) => {
+chosenEffect.style.opacity =  depthEffect.noUiSlider.get();
+});
+
+chosenEffect.addEventListener('change', (evt) => {
+    if (evt.target.value === 'marvin') {
+    depthEffect.noUiSlider.updateOptions ({
+      range: {
+        min = 0,
+        max = 100,
+      },
+      step: 1,
+    });
+  }
+  if (evt.target.value === 'phobos') {
+    depthEffect.noUiSlider.updateOptions ({
+      range: {
+        min = 0,
+        max = 3,
+      },
+      step: 0.1,
+    });
+  }
+  if (evt.target.value === 'heat') {
+    depthEffect.noUiSlider.updateOptions ({
+      range: {
+        min = 1
+        max = 3,
+      },
+      step: 0.1,
+    });
+  }
+  if (evt.target.value === 'none') {
+    depthEffect.setAttribute('disabled', true);
+  }
+  depthEffect.noUiSlider.set(1);
+});
+
 
 function changePhotoScale () {
   const maxScale = photoScaleMassive.length - 1;
@@ -87,15 +130,6 @@ function changePhotoScale () {
 pristine.addValidator(form.querySelector('[name="scale"]'),
   changePhotoScale(), 'Фотография не может быть меньше 25% от её размера и больше изначального');
 */
-/*function validateHashtags (value) {
-  if (! re.test(value)) {
-    return hashtagField.setCustomValidity(
-      `Хэш-тег должен начинается с символа # (решётка)
-
-      Хэш-тег должен состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.`,);
-  }
-  return value.length <= 5;
-} */
 
 function validateHashtags () {
   const array = hashtagField.value.toLowerCase().trim().split(' ');
