3d115b657cb8e3560bd4602d2be25bab84731a34
Kidn3y <79150932621@ya.ru>
2022-07-21 09:43:17 +0300

    Рандомное колличество комментариев

--- js/photos.js
+++ js/photos.js
@@ -2,6 +2,13 @@ import {createComment} from './comment.js';
 
 import {createPhoto} from './photo.js';
 
+import {getRandomNumber} from './util.js';
+
+const MIN_COMMENTS_COUNT = 0;
+
+const MAX_COMMENT_COUNT = 100;
+
+
 const createPhotos = () => {
   const photos = [];
 
@@ -9,7 +16,7 @@ const createPhotos = () => {
     const post = createPhoto(i + 1);
     const comments = [];
 
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < getRandomNumber(MIN_COMMENTS_COUNT, MAX_COMMENT_COUNT); j++) {
       const comment = createComment(j + 1);
       comments.push(comment);
     }
